802a0c9f0d99f1486b4de3b09d5bd37cd7e63a38
Miku Shimizu <miku@MikunoMacBook-Pro.local>
2017-03-23 20:43:26 +0900

    create gemfile & connect db

--- app.rb
+++ app.rb
@@ -1,24 +1,30 @@
 require 'sinatra'
 require 'sinatra/reloader' if development?
-# require 'rubygems'
-# require 'mysql'
-#
-# before do
-#   connection = Mysql::connect("localhost", "sinatra", "sinatra", "bookmark")
-#   connection.query("set character set utf8")
-# end
+require 'rubygems'
+require "bundler/setup"
+require 'mysql'
+require 'pry'
+
+connection = nil
+
+before do
+  connection = Mysql::connect("localhost", "sinatra", "sinatra", "bookmark")
+  connection.query("set character set utf8")
+end
 
 get '/' do
   erb :index
 end
 
 get '/list' do
-  # list = connection.query("select * from items")
-  # list.each do |li|
-  #   puts li.join ", "
-  # end
+  list = connection.query("select * from items")
+  str = ""
+  list.each do |li|
+    str += li.join(", ") + "<br>"
+  end
+  str
 end
 
-# after do
-#   connection.close
-# end
+after do
+  connection.close
+end
